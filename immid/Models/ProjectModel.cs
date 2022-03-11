157be0dcdd520bcd5350f514845992761c43e76b
Им Вадим <Badim_ipanov@mail.ru>
2022-03-11 11:51:23 +0300

    Добавил модель Project

---
+++ immid/Models/ProjectModel.cs
@@ -0,0 +1,10 @@
+﻿namespace immid.Models
+{
+    public class ProjectModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDexc { get; set; }
+    }
+}
--- immid/Program.cs
+++ immid/Program.cs
@@ -24,4 +24,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "project",
+    pattern: "{controller=Project}/{action=Index}/{id?}");
+
 app.Run();
